074e50e0c22bda55e69732f0822d7b01ae927065
jovin-sylvester <jovin.s@wizzybox.com>
2025-09-30 13:20:18 +0530

    new login page update

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -29,6 +29,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
           // Background circles (keeping your existing design)
